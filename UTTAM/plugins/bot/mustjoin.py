456dd380d1a0a8695b2761e7af9a2b873b3b1241
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2025-01-26 08:39:58 +0530

    Update mustjoin.py

--- UTTAM/plugins/bot/mustjoin.py
+++ UTTAM/plugins/bot/mustjoin.py
@@ -4,40 +4,49 @@ from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForb
 from UTTAM import app
 
 #--------------------------
-
-MUST_JOIN = "nenobots"
+MUST_JOIN = ["nenobots", "ll_BOTCHAMBER_ll", "ur_rishu_143"]
 #------------------------
+
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
     if not MUST_JOIN:
         return
-    try:
+
+    not_joined_channels = []
+    main_button = []
+
+    for channel in MUST_JOIN:
         try:
-            await app.get_chat_member(MUST_JOIN, msg.from_user.id)
+            await app.get_chat_member(channel, msg.from_user.id)
         except UserNotParticipant:
-            if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
+            if channel.isalpha():
+                link = "https://t.me/" + channel
             else:
-                chat_info = await app.get_chat(MUST_JOIN)
+                chat_info = await app.get_chat(channel)
                 link = chat_info.invite_link
-            try:
-                await msg.reply_photo(
-                    photo="https://files.catbox.moe/zfy8qm.jpg", 
-                    caption="๏ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ ʏᴇᴛ, \n\n ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ᴛʜᴇɴ ᴊᴏɪɴ ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ !",
-                    reply_markup=InlineKeyboardMarkup(
+            not_joined_channels.append(channel)
+            main_button.append(
+                [InlineKeyboardButton(f"๏Jᴏɪɴ {channel}๏", url=link)]
+            )
+
+    if not_joined_channels:
+        try:
+            await msg.reply_photo(
+                photo="https://files.catbox.moe/zfy8qm.jpg",
+                caption="๏ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ ᴀʟʟ ʀᴇQᴜɪʀᴇᴅ ᴄʜᴀɴɴᴇʟs, \n\n ᴊᴏɪɴ ᴛʜᴇᴍ ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ!",
+                reply_markup=InlineKeyboardMarkup(
+                    main_button
+                    + [
                         [
-                            [
-                                InlineKeyboardButton("๏Jᴏɪɴ๏", url=link)
-                            ],
-                            [
-                                InlineKeyboardButton("๏Jᴏɪɴ๏", url="https://t.me/ll_BOTCHAMBER_ll"),
-                                InlineKeyboardButton("๏Jᴏɪɴ๏", url="https://t.me/ur_rishu_143")
-                            ]
+                            InlineKeyboardButton("๏Jᴏɪɴ๏", url="https://t.me/ll_BOTCHAMBER_ll"),
+                            InlineKeyboardButton("๏Jᴏɪɴ๏", url="https://t.me/ur_rishu_143")
                         ]
-                    )
+                    ]
                 )
-                await msg.stop_propagation()
-            except ChatWriteForbidden:
-                pass
-    except ChatAdminRequired:
-        print(f"๏ᴘʀᴏᴍᴏᴛᴇ ᴍᴇ ᴀs ᴀɴ ᴀᴅᴍɪɴ ɪɴ ᴛʜᴇ ᴍᴜsᴛ_Jᴏɪɴ ᴄʜᴀᴛ ๏: {MUST_JOIN} !")
+            )
+            await msg.stop_propagation()
+        except ChatWriteForbidden:
+            pass
+    else:
+        # Proceed with other actions if the user has joined all channels
+        pass
